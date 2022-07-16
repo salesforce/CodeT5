f609cc3a6d6713a5fe892cf7a09e8d6996155719
Mahesh Arumugam <aumahesh@berkeley.edu>
2022-07-16 08:14:37 -0700

    directory fixes

--- finetune_mrv.sh
+++ finetune_mrv.sh
@@ -5,11 +5,11 @@ python run_gen.py --do_train --do_eval --do_eval_bleu --do_test  \
 	--tokenizer_name=Salesforce/codet5-base  \
 	--model_name_or_path /home/aumahesh/w266-summer-2022-project/data/model_mrv_best \
 	--data_dir /home/aumahesh/w266-summer-2022-project/data/code-docstring-corpus/data  \
-	--cache_path /home/aumahesh/w266-summer-2022-project/data/finetuned/cache  \
-	--output_dir /home/aumahesh/w266-summer-2022-project/data/finetuned/output  \
-	--summary_dir /home/aumahesh/w266-summer-2022-project/data/finetuned/summary \
+	--cache_path /home/aumahesh/w266-summer-2022-project/data/finetuned/codet5_mrv/cache  \
+	--output_dir /home/aumahesh/w266-summer-2022-project/data/finetuned/codet5_mrv/output  \
+	--summary_dir /home/aumahesh/w266-summer-2022-project/data/finetuned/codet5_mrv/summary \
 	--save_last_checkpoints --always_save_model \
-	--res_dir /home/aumahesh/w266-summer-2022-project/data/finetuned/results --res_fn mrv_res.txt \
+	--res_dir /home/aumahesh/w266-summer-2022-project/data/finetuned/codet5_mrv/results --res_fn mrv_res.txt \
 	--train_batch_size 16 --eval_batch_size 8 --max_source_length 256 --max_target_length 128 \
-	2>&1 | tee /tmp/log.txt
+	2>&1 | tee /home/aumahesh/w266-summer-2022-project/data/finetuned/codet5_mrv/log/log.txt
 
