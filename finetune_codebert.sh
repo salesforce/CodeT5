401b0130bbc3544d25245eb305ab84c03d9a2a92
Mahesh Arumugam <aumahesh@berkeley.edu>
2022-07-19 21:41:30 -0700

    finetune hyperparameters

--- finetune_codebert.sh
+++ finetune_codebert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python run_gen.py --do_train --do_eval --do_eval_bleu --do_test  \
 	--task summarize --sub_task python --model_type roberta --data_num -1 \
-	--num_train_epochs 1 --warmup_steps 1000 --learning_rate 5e-5 --patience 3 \
+	--num_train_epochs 3 --warmup_steps 1000 --learning_rate 5e-5 --patience 3 \
 	--tokenizer_name=roberta-base  \
 	--model_name_or_path microsoft/codebert-base \
 	--data_dir /home/aumahesh/w266-summer-2022-project/data/code-docstring-corpus/data  \
